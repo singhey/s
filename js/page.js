38c99375febd96ede4d356263d790343f95ef691
singhey <singhey2@gmail.com>
2016-12-16 16:57:20 +0530

    Update page.js

--- js/page.js
+++ js/page.js
@@ -36,15 +36,12 @@ $(window).load(function(){
 				select();
 				return writeStyle(message, message.length+10000, interval);
 			});
-			if(index==message.length-200){
-				select();
-			}
 			return setTimeout(function(){
 				return writeStyle(message, index, interval);
 			}, interval);
 		}
 	}
-	
+	select();
 	
 	writeStyle(styles, 0, 20);
 	select = function(){
@@ -52,4 +49,14 @@ $(window).load(function(){
 		$('ul.menu>li:nth-child(1)').addClass('changeBackground');
 		$('div.con0').fadeIn(300).addClass('in-out');
 	}
+		$('ul.menu>li').removeClass('changeBackground');
+		k = $(this).index();
+		var p = "con" + k;
+		$('ul.menu>li:nth-child('+($(this).index()+1)+')').addClass('changeBackground');
+		$('div.content>div').fadeOut(100).removeClass('in-out').removeClass('out-in');
+		if(k%2==0)
+			$('div.'+p).fadeIn(300).addClass('in-out');
+		else
+			$('div.'+p).fadeIn(300).addClass('out-in');
+	});
 });
