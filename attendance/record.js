e4fe909d80fb2935aa2ab4af23faec566c118a7b
singhey <singhey2@gmail.com>
2017-02-24 19:25:39 +0530

    Added SVG functions

--- attendance/record.js
+++ attendance/record.js
@@ -1,33 +1,76 @@
 window.onload = function(){
+	console.log("loaded");
 	var container = document.getElementsByClassName("bars")[0],
-		divisionsVertical = parseInt(getComputedStyle(container)["height"].replace("px", "")),
-		divisionsHorizontal = parseInt(getComputedStyle(container)["width"].replace("px", ""));
-		time = localStorage.dayLength.split(" | ").length,
-		eachMinuteVertical = parseFloat(divisionsVertical/((17 - 9) * 60)).toFixed(2),
-		eachMinuteHorizontal = Math.floor(divisionsHorizontal/time);
-		if(eachMinuteHorizontal >=6) eachMinuteHorizontal = 6;
-	addBars(container, eachMinuteVertical, eachMinuteHorizontal);
+		svg = document.getElementsByTagName("svg")[0];
+	function reAlign(){
+		var h = parseInt(getComputedStyle(container)["height"].replace("px", "")),
+			w = parseInt(getComputedStyle(container)["width"].replace("px", ""));
+			svg = document.getElementsByTagName("svg")[0];
+		svg.style.height = h - 20;
+		svg.style.width = w;
+	}
+	window.onresize = function(){reAlign();getPoints();alignCircle();};
+	getPoints();
+	reAlign();
+	addCircles();
 };
+var data = [];
+function makeSVGEl(tag, attrs) {
+    var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs) {
+      el.setAttribute(k, attrs[k]);
+    }
+    return el;
+}
 
-function getHeight(a, h){
+function getPoints(){
+	var points = '',
+		time = localStorage.dayLength,
+		svg = document.getElementsByTagName("svg")[0],
+		container = document.getElementsByClassName("bars")[0],
+		h = parseInt(getComputedStyle(container)["height"].replace("px", "")),
+		w = parseInt(getComputedStyle(container)["width"].replace("px", "")),
+		timeArr = time.split("|"),
+		pSpace = parseFloat(w/(timeArr.length)).toFixed(2),
+		pHeight = parseFloat(h/((17 - 9) * 60)).toFixed(2);
+	points = '0 ,'+h;
+	data = [];
+	for(var i = 0;i <timeArr.length; i++){
+		var x = pSpace * i,
+			y = parseFloat(h - (getVertical(timeArr[i])*pHeight)).toFixed(2);;
+		if(y<5)y= 8;
+		points = points+' '+x+','+y;
+		data.push({cx:x, r:4, cy:y, title:timeArr[i]});
+	}
+	points = points+' '+w+','+h;
+	svg.children[0].setAttribute("points", points);
+}
+
+function addCircles(){
+	var svg = document.getElementsByTagName("svg")[0];
+	data.forEach(function(c){
+		svg.appendChild(makeSVGEl("circle", {
+			cx:c.cx,
+			r:c.r,
+			cy:c.cy,
+			title:c.title
+		}));	
+	});
+	
+}
+
+function alignCircle(){
+	var circle = document.getElementsByTagName("circle");
+	for(var i = 0;i <data.length; i++){
+		circle[i].setAttribute("cx", data[i].cx);
+		circle[i].setAttribute("cy", data[i].cy);
+	}
+}
+
+function getVertical(a){
 	var x = a.split(":"),
 		hours = x[0],
 		minutes = x[1];
 		if(hours>=17){hours = 17;minutes= 0;}
 	return parseInt((hours - 9)* 60) + parseInt(minutes);
-}
-
-function addBars(el, h, w){
-	var string = localStorage.dayLength,
-		arr = string.split("|");
-
-	for(var i = 0;i <arr.length; i++){
-		var height = getHeight(arr[i], h),
-			div = document.createElement('div');
-		div.setAttribute("class", "bar");
-		div.style.height = (height * h)+'px';
-		div.style.width = w+'px';
-		div.style.left = ((w * i))+'px';
-		el.appendChild(div);
-	}
 }
