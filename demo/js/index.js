61d42233003afb76fdf93c881047f688509b7d2d
singhey <singhey2@gmail.com>
2017-08-13 03:02:01 +0530

    Add files via upload

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,68 +1,27 @@
+$.fn.inView = function(){
+    //Window Object
+    var win = $(window);
+    //Object to Check
+    obj = $(this);
+    //the top Scroll Position in the page
+    var scrollPosition = win.scrollTop();
+    //the end of the visible area in the page, starting from the scroll position
+    var visibleArea = win.scrollTop() + win.height();
+    //the end of the object to check
+    var objEndPos = obj.offset().top;
+    return(visibleArea >= objEndPos && scrollPosition <= objEndPos ? true : false);
+};
 $(function(){
-	var width;
-	function realign(){
-		width = $(window).innerWidth();
-		$(".t__holder").css({'width': 4 * width+"px"});
-		$(".t").css({'width':width+"px"});
-		console.log($('.t').width() + " "+ $(".t__holder").width());
-		$(".tabs-holder").css({'width': $('.tab').length * width+"px"});
-		$(".tab").css({'width':width+"px"});
-	}
-
-	$('.services-tab').click(function(){
-		$('.services-tab').removeClass("active");
-		$(this).addClass("active");
-		var i = $(this).index(),
-			el = $('.tabs-holder');
-		move(el,i);
-	});
-
-	function move(el, i){
-		$(el).css({'margin-left':-i*width+'px'});
-	}
-
-	var  tSlide = 0; //testimonial slide number
-
-
-	$('.testimonials> .forward-arrow').click(function(){
-		if(tSlide < $(".t").length - 1){
-			tSlide++;
-			var el = $('.t__holder');
-			move(el, tSlide);
-		}
-	});
-
-	$('.testimonials> .backward-arrow').click(function(){
-		if(tSlide > 0){
-			tSlide--;
-			var el = $('.t__holder');
-			move(el, tSlide);
-		}
-	});
-
-	//for colors in each portfolio tab
 	var colors = ['#ff9a9e', '#2ae', '#a1c4fd', '#667eea', '#89f7fe', '#13547a', '#434343', '#4ae'];
 	$('.folio-column > div').each(function(i){
  		$(this).css({'background':colors[i]});
-       	console.log(i+"entered");
 	});
-
-	$(window).resize(realign);
-	realign();
-
-	//call once the page has loaded
-	setTimeout(function(){
-		$('.flipX').removeClass("flipX");
-		$('.be-left').addClass('fade-in-right visible');
-	},300);
-
-
+	$('.toggler').click(function(){
+		$('body').toggleClass("menu-visible");
+	});
 	$(window).scroll(function(){
 		if($('.folio-column').inView()){
-			$('.be-down').addClass("fade-in");
-		}
-		if($('.t').inView()){
-			$('.t').addClass("fade-in visible");
+			$('.folio-column').addClass("fade-in");
 		}
 		if($('.team').inView()){
 			$('.member-card').addClass("fade-in");
