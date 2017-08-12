9ffc6313e31aa2dcba71533b18ba7df3a67f0ff9
singhey <singhey2@gmail.com>
2017-08-13 03:06:07 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$.fn.inView = function(){
+    //Window Object
+    var win = $(window);
+    //Object to Check
+    obj = $(this);
+    //the top Scroll Position in the page
+    var scrollPosition = win.scrollTop();
+    //the end of the visible area in the page, starting from the scroll position
+    var visibleArea = win.scrollTop() + win.height();
+    //the end of the object to check
+    var objEndPos = obj.offset().top;
+    return(visibleArea >= objEndPos && scrollPosition <= objEndPos ? true : false);
+};
+$(function(){
+	var colors = ['#ff9a9e', '#2ae', '#a1c4fd', '#667eea', '#89f7fe', '#13547a', '#434343', '#4ae'];
+	$('.folio-column > div').each(function(i){
+ 		$(this).css({'background':colors[i]});
+	});
+	$('.toggler').click(function(){
+		$('body').toggleClass("menu-visible");
+	});
+	$(window).scroll(function(){
+		if($('.folio-column').inView()){
+			$('.folio-column').addClass("fade-in");
+		}
+		if($('.team').inView()){
+			$('.member-card').addClass("fade-in");
+		}
+	});
+});
