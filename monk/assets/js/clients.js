5a7d39b33007fdcbb8103d850963229bdfe4c8c5
singhey <singhey2@gmail.com>
2018-02-21 01:40:59 +0530

    Add files via upload

---
+++ monk/assets/js/clients.js
@@ -0,0 +1,10 @@
+$(function(){
+
+	var web = $('#web-design-svg').drawsvg();
+	web.drawsvg('animate');
+	console.log(web.drawsvg);
+
+
+	var content = $('#content').drawsvg();
+	content.drawsvg('animate');
+});
