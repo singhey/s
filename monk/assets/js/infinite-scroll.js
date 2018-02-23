a997d799b17a46c26fab36d46eb7a8a3845f3fd7
singhey <singhey2@gmail.com>
2018-02-23 21:13:14 +0530

    Add files via upload

--- monk/assets/js/infinite-scroll.js
+++ monk/assets/js/infinite-scroll.js
@@ -1,9 +1,29 @@
 $(function(){
 
-	console.log("called");
-	console.log($('.slider-customer > li').width());
+
+	slideWidth = ($('.slider-customer > li').width() + 48);
+	count = 1;
+	//set up width of parent slider
 	$('.slider-customer').css({
 		"width": (($('.slider-customer > li').width() + 48) * $('.slider-customer > li').length)+"px",
 	});
+	window.setInterval( function() {
+		//console.log("hello", slideWidth, count);
+		$('.slider-customer').css({
+			"transition": "transform 0s linear",
+			"transform": "translateX(0px)",
+		});
+		var p = $(".slider-customer > li:first-child");
+		p.remove();
+		$('.slider-customer').append(p);
+		window.setTimeout(function(){
+			$('.slider-customer').css({
+				"transition": "transform 2s linear",
+				"transform": "translateX(-"+(slideWidth * count)+"px)",
+			});
+		}, 20);
+		//console.log(p);
+		//console.log($('.slider-customer>li').length);
+	}, 2000);
 
 });	
