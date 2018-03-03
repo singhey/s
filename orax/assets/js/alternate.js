d988540972d5b4465c38a4a9a05aa39842a2610f
singhey <singhey2@gmail.com>
2018-03-03 19:04:48 +0530

    Add files via upload

--- orax/assets/js/alternate.js
+++ orax/assets/js/alternate.js
@@ -6,7 +6,7 @@ $(function(){
 		$(this).addClass("active");
 
 		var contentIndex = $(this).attr('data-content-index');
-		var _content = $('ol.sub-content[data-slide="'+slide+'"]');
+		var _content = $('ul.sub-content[data-slide="'+slide+'"]');
 		//var content;
 		//console.log(_content.length, _content);
 		$(_content).removeClass("active");
@@ -19,6 +19,33 @@ $(function(){
 
 	});
 
-	
+	//keep revolving the active slides rotatory images
+
+	function rotatorySlide() {
+
+		var _slides = $('.slide.active').find('.images-rotator').children();
+		console.log(_slides);
+		for(var i = _slides.length - 1; i >= 0; i--) {
+			var pos = $(_slides[i]).attr("data-rotator-slide");
+			console.log(pos);
+			$(_slides[i]).removeClass("pos-"+pos);
+			var updatedPos = (pos + 1) % _slides.length;
+
+			if(updatedPos == 0) {
+				$(_slides[i]).addClass("switch");
+			}else{
+				$(_slides[i]).removeClass("switch");
+			}
+
+			$(_slides[i]).addClass("pos-"+updatedPos);
+			$(_slides[i]).attr("data-rotator-slide", updatedPos);
+
+
+		}
+		window.setTimeout(function(){rotatorySlide();}, 5000);
+
+	}
+
+	rotatorySlide();
 
 });
