eec0dfec605170aba588586b81c4ca7487ed8d05
singhey <singhey2@gmail.com>
2018-03-04 23:10:16 +0530

    Add files via upload

--- orax/assets/js/index.js
+++ orax/assets/js/index.js
@@ -1,31 +1,5 @@
 $(function(){
-	$('.text-ground').mousemove(function(e){
-		var x = e.pageX;
-		var y = e.pageY;
-		const maxTransition= {
-			x: 50,
-			y: 50,
-		};
-		x = (e.pageX / $(window).innerWidth());
-		y = e.pageY / $(window).innerHeight();
-		//console.log(x);
-		if(x > 0.5) {
-			x = (x - 0.5) * maxTransition.x;
-			x*=-1;
-		}else {
-			x = Math.abs(0.5 - x) * maxTransition.x;
-		}
-
-		if(y > 0.5) {
-			y = (y - 0.5) * maxTransition.y;
-			y*=-1;
-		}else {
-			y = Math.abs(y - 0.5) * ( maxTransition.y);
-		}
-		$(this).find('.text-holder').css({
-			'transform': 'translate('+x+'px, '+y+'px)'
-		});
-	});
+	Ripple.init();
 
 	
 });
