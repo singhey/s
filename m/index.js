dec8f6c917c910b4b183d037559470b1bd8d9d5c
Abhishek Singh <singhey2@gmail.com>
2020-01-23 16:39:35 +0530

    Changed download format

--- m/index.js
+++ m/index.js
@@ -200,7 +200,7 @@ window.addEventListener('load', function() {
 
 function download() {
   //e.preventDefault();
-  document.getElementById("downloader").download = "image.jpeg";
+  document.getElementById("downloader").download = "image.jpg";
   console.log("hello")
   document.getElementById("downloader").href = document.querySelector("canvas").toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 }
